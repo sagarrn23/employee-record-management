4e7cd1f88ad28f97828ac7905627540274eebe15
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2019-09-30 00:46:31 +0530

    Updates

---
+++ precache-manifest.0bfb8d090cc871c111521850f5872673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a01be2dea0e910e2beb825dc645417f",
+    "url": "/employee-record-management/index.html"
+  },
+  {
+    "revision": "45f20f7d2d46276ea282",
+    "url": "/employee-record-management/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5855a68a1e30487be3db",
+    "url": "/employee-record-management/static/css/main.ab60237f.chunk.css"
+  },
+  {
+    "revision": "45f20f7d2d46276ea282",
+    "url": "/employee-record-management/static/js/2.16f7985c.chunk.js"
+  },
+  {
+    "revision": "5855a68a1e30487be3db",
+    "url": "/employee-record-management/static/js/main.e0d431ce.chunk.js"
+  },
+  {
+    "revision": "7723563867836102a9d9",
+    "url": "/employee-record-management/static/js/runtime-main.968f3562.js"
+  }
+]);
